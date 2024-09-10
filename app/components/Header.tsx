6dea85a0a6d1d92fd973f44d46e60877bf2e87ce
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-09-10 17:28:24 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   app/components/Header.tsx
    	modified:   app/components/Navbar.tsx
    	modified:   app/page.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+import { Navbar } from "./Navbar";
+
+export default function Header() {
+  return (
+    <div className="h-screen">
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import { Navbar } from "./components/Navbar";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-[300vh]">
       <Navbar />
-      <header className="flex justify-center m-8"></header>
-    </>
+      <Header />
+    </div>
   );
 }
