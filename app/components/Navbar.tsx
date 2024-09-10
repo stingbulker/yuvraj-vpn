ab95e24a5217773fa643b5c09442298c907579dd
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-09-10 15:40:37 +0530

    new file:   app/components/Navbar.tsx 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components.json 	new file:   components/ui/button.tsx 	new file:   components/ui/navigation-menu.tsx 	new file:   lib/utils.ts 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   public/v.svg 	modified:   tailwind.config.ts

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center sticky top-6">
+    <NavigationMenu className="flex w-full py-1 px-1 rounded-full bg-neutral-900 border-solid border-2 border-neutral-800 ">
+      <NavigationMenuList className="flex gap-6">
+        <NavigationMenuItem className="flex justify-center">
+          <NavigationMenuLink
+            href="/"
+            className="flex gap-2 items-center justify-center px-3"
+          >
+            <Image src="/v.svg" width={25} height={25} alt="" />
+            Vaguard
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/" className="text-white">
+            Home
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/" className="text-neutral-500 ">
+            About Us
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/" className="text-neutral-500 ">
+            Features
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/" className="text-neutral-500 ">
+            Pricing
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Button className="rounded-full flex justify-center font-bold">
+            Sign Up
+          </Button>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu></div>
+  );
+};
