63c8ec3651b4f1f993388012d0b79bf781805df6
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-09-12 15:33:29 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   app/components/Header.tsx
    	new file:   app/fonts.ts
    	deleted:    app/fonts/GeistMonoVF.woff
    	deleted:    app/fonts/GeistVF.woff
    	modified:   package.json
    	modified:   pnpm-lock.yaml

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,50 +1,112 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { CirclePlay } from "lucide-react";
-import { Card, CardContent } from "@/components/ui/card"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { vt } from "../fonts";
+
+const countries = [
+  {
+    country: "India",
+    flag: "/india.png",
+  },
+  {
+    country: "USA",
+    flag: "/usa.png",
+  },
+  {
+    country: "Canada",
+    flag: "/canada.png",
+  },
+  {
+    country: "Singapore",
+    flag: "/singapore.png",
+  },
+  {
+    country: "Japan",
+    flag: "/japan.png",
+  },
+  {
+    country: "China",
+    flag: "/china.png",
+  },
+  {
+    country: "UK",
+    flag: "/uk.png",
+  },
+];
 
 export default function Header() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-20">
       {/* <div className="w-[75vw] h-[75vw] rounded-full bg-neutral-900 border-solid border-2 border-neutral-800"></div> */}
       <div className="flex flex-col items-center gap-8">
         <div className="flex flex-col items-center">
-        <h1 className="text-8xl ">Ultimate Online Privacy</h1>
-        <h1 className="text-8xl text-neutral-500 ">with Our VPN</h1></div>
+          <h1 className="text-8xl ">Ultimate Online Privacy</h1>
+          <h1 className="text-8xl text-neutral-500 ">with Our VPN</h1>
+        </div>
         <div className="flex flex-col items-center text-neutral-500">
-        <p>
-          Safeguard your data, access global content, and enjoy fast, secure
-        </p>
-        <p>browsing. Our VPN app offers top-notch security.</p></div>
+          <p>
+            Safeguard your data, access global content, and enjoy fast, secure
+          </p>
+          <p>browsing. Our VPN app offers top-notch security.</p>
+        </div>
         <div className="flex gap-2 ">
-          <Button size={"lg"} variant='yellow'>Get Started</Button>
-          <Button size={"lg"} className="flex gap-[.35rem] font-normal" variant='grey'><CirclePlay size={18} />Tutorial</Button>
+          <Button size={"lg"} variant="yellow">
+            Get Started
+          </Button>
+          <Button
+            size={"lg"}
+            className="flex gap-[.35rem] font-normal"
+            variant="grey"
+          >
+            <CirclePlay size={18} />
+            Tutorial
+          </Button>
         </div>
       </div>
-      <div className="">
-      <Carousel className="w-full max-w-sm">
-      <CarouselContent className="-ml-1">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      <div className="flex w-full">
+        <Carousel
+          className="w-full"
+          plugins={[
+            // Autoplay({
+            //   delay: 4000,
+            // }),
+          ]}
+        >
+          <CarouselContent className="w-full">
+            {countries.map((country, index) => (
+              <CarouselItem key={index} className="w-[1/7]">
+                <Card>
+                  <CardHeader>
+                    <div className="flex">
+                      <div className="w-10 h-10 bg-neutral-300 rounded-full"></div>
+                    </div>
+                  </CardHeader>
+                  <CardFooter className="flex flex-col gap-2">
+                    <p>Location</p>
+                    <p className={`${vt.className} text-4xl`}>{country.country}</p>
+                  </CardFooter>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious />
+          <CarouselNext /> */}
+        </Carousel>
       </div>
     </div>
   );
---
+++ app/fonts.ts
@@ -0,0 +1,6 @@
+import { VT323 } from 'next/font/google'
+ 
+export const vt = VT323({
+  weight: ['400'],
+  subsets: ["latin"]
+})
